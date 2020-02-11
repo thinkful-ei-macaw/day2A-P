50064d2d46d90080c6fd7bcc16b77379315c9178
Patrick Nguyen <patrick.nguyen154@gmail.com>
2020-02-11 13:01:39 -0700

    nextone

--- objectdrill1.js
+++ objectdrill1.js
@@ -38,10 +38,14 @@ function personMaker() {
     lastName: 'Jones',
     // replace `null` with a function that uses self reference to return
     // full name
-    fullName: null,
+    fullName: function() {
+        return `${this.firstName} ${this.lastName}`;
+    } ,
   };
   return person;
 }
+
+console.log(personMaker());
 
 const sampleObj = {
   foo: 'foo',
