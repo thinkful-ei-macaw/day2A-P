ada2a52785fc69c4f905be1ec44f564c45b66c61
Patrick Nguyen <patrick.nguyen154@gmail.com>
2020-02-11 10:08:50 -0700

    first commit

---
+++ objectdrill1.js
@@ -0,0 +1,31 @@
+'use strict';
+
+function createMyObject() {
+
+}
+
+function updateObject(obj) {
+
+}
+
+function personMaker() {
+  var person = {
+    firstName: 'Paul',
+    lastName: 'Jones',
+    // replace `null` with a function that uses self reference to return
+    // full name
+    fullName: null,
+  };
+  return person;
+}
+
+const sampleObj = {
+  foo: 'foo',
+  bar: 'bar',
+  bizz: 'bizz',
+  bang: 'bang',
+};
+  
+function keyDeleter(obj) {
+  // your code here
+}
