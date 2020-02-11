fd4392d5da73c19600c6c763baf5b081788a0d53
Agatha North <rouborn@gmail.com>
2020-02-11 12:31:01 -0800

    ez

--- objectnew.js
+++ objectnew.js
@@ -1,5 +1,6 @@
 'use strict';
 
+//1
 let loaf = {
     flour: 300,
     water: 210,
@@ -11,3 +12,17 @@ console.log(loaf.water)
 loaf.hydration = (loaf.water / loaf.flour ) * 100;
 
 console.log(loaf.hydration);
+
+//2
+
+const objTwo = {
+    foo: 'foo',
+    bar: 'bar',
+    fum: 'fum',
+    quux: 'quux',
+    spam: 'spam'
+}
+
+for (const property in objTwo){
+    console.log(`${property}: ${objTwo[property]}`)
+}
