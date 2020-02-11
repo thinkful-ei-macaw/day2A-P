38ab9276f0b6faedcf355d0ea5b1546d69dd9437
Patrick Nguyen <patrick.nguyen154@gmail.com>
2020-02-11 12:08:57 -0700

    passback

--- loops.js
+++ loops.js
@@ -1,22 +1,32 @@
+'use strict';
 
 function max(numbers) {
-    let i = 0; 
-    let largest = 0;
-    while (i < numbers.length){
-        if(numbers[i] > largest){
-            largest = numbers[i];
-        }
-        i++;
+  let i = 0; 
+  let largest = numbers[0];
+  while (i < numbers.length){
+    if(numbers[i] > largest){
+      largest = numbers[i];
     }
-    return largest;
+    i++;
+  }
+  return largest;
 }
 console.log(max([10,21,5,8,12]))
   
 
 function min(numbers) {
-    let i = 0;
+  let i = 0;
+  let smallest = numbers[0];
+  while(i < numbers.length){
+    if(numbers[i] < smallest){
+      smallest = numbers[i];
+    }
+    i++;
+  }
+  return smallest;
 }
 
+console.log(min([10,21,5,8,12]))
 
 function average(numbers) {
 }
