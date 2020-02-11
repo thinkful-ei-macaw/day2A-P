c7f7ec9db0d34b351a5b4a3df3ed743ea2c4444c
Agatha North <rouborn@gmail.com>
2020-02-11 09:40:18 -0800

    loops update

---
+++ loops.js
@@ -0,0 +1,22 @@
+
+function max(numbers) {
+    let i = 0; 
+    let largest = 0;
+    while (i < numbers.length){
+        if(numbers[i] > largest){
+            largest = numbers[i];
+        }
+        i++;
+    }
+    return largest;
+}
+console.log(max([10,21,5,8,12]))
+  
+
+function min(numbers) {
+    let i = 0;
+}
+
+
+function average(numbers) {
+}
